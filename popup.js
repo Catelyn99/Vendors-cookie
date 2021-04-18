3b754d1a18cf9ff5e4d556f6b0992b71f06e3560
Catelyn99 <katarzynasroka99@gmail.com>
2021-04-18 18:59:18 +0200

    Init

---
+++ popup.js
@@ -0,0 +1,99 @@
+document.querySelector('.accept-btn').addEventListener("click", () => {
+    function setCookie(vendorsId) {
+        let expires = "";
+        const date = new Date();
+        date.setTime(date.getTime() + (24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+        document.cookie = `vendors=${vendorsId}${expires}; path=/`;
+    }
+    let selectedVendorsId = [...document.querySelectorAll('.vendor-checkbox:checked')].map(element => element.name);
+    setCookie(selectedVendorsId);
+});
+
+(() => {
+    const getJSON = (url, callback) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', url, true);
+        xhr.responseType = 'json';
+        xhr.onload = () => {
+            const status = xhr.status;
+            if (status === 200) {
+                callback(null, xhr.response);
+            } else {
+                callback(status, xhr.response);
+            }
+        };
+        xhr.send();
+    };
+
+    getJSON('https://optad360.mgr.consensu.org/cmp/v2/vendor-list.json',
+        (err, data) => {
+            if (err !== null) {
+                alert('Something went wrong: ' + err);
+            } else {
+                let vendorsHtml = Object.values(data.vendors).map(vendor =>
+                    `<div class='vendor-box'>
+                   <input class='vendor-checkbox' type="checkbox" id="vendor-${vendor.id}" name="${vendor.id}"/>
+                   <label for="vendor-${vendor.id}" class="vendor-name">${vendor.name}</label>
+                   <div class="vendor-policy">
+                     <a class="policy-url" href="${vendor.policyUrl}" target="_blank">
+                       PolicyUrl
+                     </a>
+                     <a class="mobile-policy" href="${vendor.policyUrl}" target="_blank">
+                       Policy
+                     </a>
+                   </div>
+                 </div>`
+                );
+                document.querySelector('#popup-vendors').innerHTML = vendorsHtml.join('');
+            }
+        });
+})();
+
+(() => {
+    const closePopup = () => {
+        document.querySelector('#popup').style.display = 'none';
+        document.body.style.overflow = 'initial';
+        document.querySelector('#app').style.filter = 'none';
+    };
+
+    const showPopup = () => {
+        const getCookie = (cookieName) => {
+            const name = cookieName + "=";
+            const decodedCookie = decodeURIComponent(document.cookie);
+            const cookies = decodedCookie.split(';');
+            for (let cookie of cookies) {
+                while (cookie.charAt(0) == ' ') {
+                    cookie = cookie.substring(1);
+                }
+                if (cookie.indexOf(name) == 0) {
+                    return cookie.substring(name.length, cookie.length);
+                }
+            }
+            return "";
+        };
+
+        if (getCookie('vendors')) {
+            closePopup();
+        };
+    };
+
+    const attachClosePopup = (selector) => {
+        document.querySelector(selector).addEventListener("click", () => {
+            closePopup();
+        });
+    };
+
+    showPopup();
+    attachClosePopup('.accept-btn');
+    attachClosePopup('.reject-btn');
+})();
+
+document.querySelector('.select-all-btn').addEventListener("click", () => {
+    const checkboxes = [...document.querySelectorAll('.vendor-checkbox')];
+    if (checkboxes.every(checkbox => checkbox.checked)) {
+        checkboxes.forEach(checkbox => checkbox.checked = false);
+    } else {
+        checkboxes.forEach(checkbox => checkbox.checked = true);
+    }
+});
