6281ba53f40c60e33d7db7f621fb15318193b3f9
Catelyn99 <katarzynasroka99@gmail.com>
2021-04-22 16:57:42 +0200

    updated cookie

--- popup.js
+++ popup.js
@@ -40,18 +40,27 @@ const createPopup = () => {
         }
     });
 
-    document.querySelector('.accept-btn').addEventListener("click", () => {
-        function setCookie(vendorsId) {
-            let expires = "";
-            const date = new Date();
-            date.setTime(date.getTime() + (24 * 60 * 60 * 1000));
-            expires = "; expires=" + date.toUTCString();
-            document.cookie = `vendors=${vendorsId}${expires}; path=/`;
-        }
-        let selectedVendorsId = [...document.querySelectorAll('.vendor-checkbox:checked')].map(element => element.name);
-        setCookie(selectedVendorsId);
-    });
 
+    const createCookie = selector => {
+        document.querySelector(selector).addEventListener("click", () => {
+            function setCookie(vendorsId) {
+                let expires = "";
+                const date = new Date();
+                date.setTime(date.getTime() + (24 * 60 * 60 * 1000));
+                expires = "; expires=" + date.toUTCString();
+                if (vendorsId == 0) {
+                    document.cookie = `vendors= 0 ${expires}; path=/`;
+                } else {
+                    document.cookie = `vendors=${vendorsId}${expires}; path=/`;
+                }
+            }
+            let selectedVendorsId = [...document.querySelectorAll('.vendor-checkbox:checked')].map(element => element.name);
+            setCookie(selectedVendorsId);
+        });
+    };
+
+    createCookie('.accept-btn');
+    createCookie('.reject-btn');
     attachClosePopup('.accept-btn');
     attachClosePopup('.reject-btn');
 
